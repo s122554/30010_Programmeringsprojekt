11d12a1048b748ab4730e36d544fb8f2e1a680a9
s122554 <s122554@student.dtu.dk>
2014-06-17 10:11:55 +0200

    Striker

---
+++ asin.h
@@ -0,0 +1,17 @@
+// =====================================================================
+//
+//	Exported by Cearn's excellut v1.0
+//	(comments, kudos, flames to daytshen@hotmail.com)
+//
+// =====================================================================
+
+#ifndef ASIN_H
+#define ASIN_H
+
+// === LUT SIZES ===
+#define ARCSIN_SIZE 129
+
+// === LUT DECLARATIONS ===
+extern const signed short ARCSIN[129];
+
+#endif	// ASIN_H
